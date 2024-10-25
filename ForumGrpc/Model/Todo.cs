52038871691e9a746b8deb6d82681beed58ff857
Nathaniel Andrian <nathaniel.andrian@binus.edu>
2024-10-25 16:29:20 +0700

    initial commit

---
+++ ForumGrpc/Model/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumGrpc.Model;
+
+public class Todo
+{
+    public int  Id { get; set; }
+    public string? Title { get; set; }
+    public string?  Description { get; set; }
+    public string Status { get; set; } = "New";
+}
---
+++ ForumGrpc/Services/TodoService.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+
+namespace ForumGrpc.Services;
+
+public class TodoService: Todo.TodoBase
+{
+    
+    // add db context
+    public TodoService()
+    {
+        
+    }
+
+    public override async Task<CreateTodoResponse> CreateTodo(CreateTodoRequest request, ServerCallContext context)
+    {
+        if (request.Title == string.Empty || request.Description == string.Empty)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "you must filll a valid object"));
+        }
+
+        var todoItem = new Model.Todo
+        {
+            Title = request.Title,
+            Description = request.Description
+        };
+        
+        //TODO: ADD TO DB
+
+        return await Task.FromResult(new CreateTodoResponse
+        {
+            Id = 1
+        });
+        
+    }
+
+    public override async Task<ReadTodoResponse> ReadTodo(ReadTodoRequest request, ServerCallContext context)
+    {
+        if (request.Id == 0)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "resource index must be greater than 0"));
+        }
+        
+        var todoItem = new Model.Todo
+        {
+            Id = 1,
+            Title = "test",
+            Description = "test description"
+        };
+
+        return new ReadTodoResponse
+        {
+            Id = todoItem.Id,
+            Description = todoItem.Description,
+            Title = todoItem.Title,
+            ToDoStatus = todoItem.Status
+        };
+    }
+}
